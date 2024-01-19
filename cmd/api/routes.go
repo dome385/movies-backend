45012f12fdab4faddb47dbaf566e252f91846a17
dome385 <dominik.niestroj@stud.hshl.de>
2024-01-19 23:17:05 +0100

    cors.options

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,14 +5,27 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func (app *application) routes() http.Handler{
+	
+	options := cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}
 	//create a router mux
 	mux := chi.NewRouter()
 
 	mux.Use(middleware.Recoverer)
-	mux.Use(app.enableCORS)
+	//mux.Use(app.enableCORS)
+	mux.Use(cors.Handler(options))
 
 	mux.Get("/", app.Home)
 	mux.Get("/movies", app.AllMovies)
