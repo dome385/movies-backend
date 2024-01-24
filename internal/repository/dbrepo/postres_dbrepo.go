b06bf5678669b2bdf4610ff036b8ae281dcaf588
dome385 <dominik.niestroj@stud.hshl.de>
2024-01-24 13:16:51 +0100

    read movies

--- internal/repository/dbrepo/postres_dbrepo.go
+++ internal/repository/dbrepo/postres_dbrepo.go
@@ -96,7 +96,7 @@ func (m *PostgresDBRepo) OneMovie(id int) (*models.Movie, error) {
 	// get genres, if any
 	query = `select g.id, g.genre from movies_genres mg 
 		left join genres g on (mg.genre_id = g.id)
-		where mg.movie_id $1
+		where mg.movie_id = $1
 		order by g.genre`
 
 	// querycontext bei mögliche mehreren objekten, in diesem beispiel können ein film mehrere genres haben
@@ -210,7 +210,7 @@ func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre
 	// get genres, if any
 	query = `select g.id, g.genre from movies_genres mg 
 		left join genres g on (mg.genre_id = g.id)
-		where mg.movie_id $1
+		where mg.movie_id= $1
 		order by g.genre`
 
 	// querycontext bei mögliche mehreren objekten, in diesem beispiel können ein film mehrere genres haben
@@ -241,7 +241,7 @@ func (m *PostgresDBRepo) OneMovieForEdit(id int) (*models.Movie, []*models.Genre
 
 	var allGenres []*models.Genre
 
-	query = "select id, genres, from genres order by genre"
+	query = "select id, genre from genres order by genre"
 	gRows, err := m.DB.QueryContext(ctx, query)
 	if err != nil {
 		return nil, nil, err
